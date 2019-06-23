87507d76357fafee1a67d39026cea3857db56c66
bibekpreet99 <bibekpreet99@gmail.com>
2019-06-23 18:25:01 +0530

    tweaked some things

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,31 +2,31 @@ import React from "react";
 import "./Card.css"
 
 
-const Card = (props)=>{
+const Card = ({props})=>{
     return(
         <div className="tc">
             <div className="card container bs3">
-                <button className="pa3 grow ma2" onClick={props.props.press} >GENERATE</button><br/>
+                <button className="pa3 grow ma2" onClick={props.press} >GENERATE</button><br/>
                 <ul className="form">
                     <li>
                         <label htmlFor="name">Name:</label>
-                        <input type="text" value={`${props.props.data.name?props.props.data.name:""}`} readOnly/><br/>
+                        <input type="text" value={`${props.data.name?props.data.name:""}`} readOnly/><br/>
                     </li>
                     <li>
                         <label htmlFor="height">Height:</label>
-                        <input type="text" value={`${props.props.data.height?props.props.data.height:""}`} readOnly/><br/>
+                        <input type="text" value={`${props.data.height?props.data.height+" cm":""}`} readOnly/><br/>
                         </li>
                     <li>
                         <label htmlFor="hair color">Hair Color:</label>
-                        <input type="text" value={`${props.props.data.hair_color?props.props.data.hair_color:""}`} readOnly/><br/>
+                        <input type="text" value={`${props.data.hair_color?props.data.hair_color:""}`} readOnly/><br/>
                     </li>
                     <li>
                         <label htmlFor="gender">Gender:</label>
-                        <input type="text" value={`${props.props.data.gender?props.props.data.gender:""}`} readOnly/><br/>
+                        <input type="text" value={`${props.data.gender?props.data.gender:""}`} readOnly/><br/>
                     </li>
                     <li>
-                        <label htmlFor="mass">Mass:</label>
-                        <input type="text" value={`${props.props.data.mass?props.props.data.mass:""}`} readOnly/><br/>
+                        <label htmlFor="weight">Weight:</label>
+                        <input type="text" value={`${props.data.mass?props.data.mass+" kg":""}`} readOnly/><br/>
                     </li>
                 </ul>
             </div>
